9fb16d5e44b439c2aaddc610ac0bee036dfc6e64
Rahul <rahulyiet@gmail.com>
2018-07-24 09:49:08 -0700

    first commit

---
+++ app/src/main/java/com/example/rahulyiet/alarmclock/AlarmReceiver.java
@@ -0,0 +1,27 @@
+package com.example.rahulyiet.alarmclock;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+//set alarm default ringtone
+
+public class AlarmReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null)
+        {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
+}
---
+++ app/src/main/java/com/example/rahulyiet/alarmclock/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.rahulyiet.alarmclock;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TimePicker alarmTimePicker;
+    PendingIntent pendingIntent;
+    AlarmManager alarmManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        //alarm manager service
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+    }
+
+    //click function
+    public void OnToggleClicked(View view) {
+
+
+        //set time date day for alarm
+       long time;
+       if(((ToggleButton)view).isChecked())
+       {
+           Toast.makeText(MainActivity.this, "ALARM ON", Toast.LENGTH_SHORT).show();
+           Calendar calendar = Calendar.getInstance();
+           calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+           calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+           Intent intent = new Intent(this, AlarmReceiver.class);
+           pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        //change time in miiliseconds
+           time = (calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
+           if(System.currentTimeMillis()>time)
+           {
+               if (calendar.AM_PM == 0)
+                   time = time + (1000*60*60*12);
+               else
+                   time = time + (1000*60*60*24);
+           }
+
+           //Repeat the alarm if not stop
+           alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
+       }
+       else
+       {
+           alarmManager.cancel(pendingIntent);
+           Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
+       }
+    }
+}
